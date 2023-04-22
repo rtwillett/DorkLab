df23b7f1c7dd363767e3067727c6d22f8e260dc0
rtwillett <ryan.willett@gmail.com>
2023-04-21 20:50:36 -0400

    Fixed MC between Ally and Ryan

--- modules/build_substring.py
+++ modules/build_substring.py
@@ -1,4 +1,4 @@
-import modules.build_substring_yandex
+# import modules.build_substring_yandex
 import numpy as np
 import pandas as pd
 import spacy
@@ -97,7 +97,44 @@ class BuildSubstring:
                 
         return res
 
+import modules.build_substring
 
+class BuildStringYandex(BuildSubstring):
+    
+    def __init__(self, data):
+        super().__init__(data)
+
+        self.data = data
+        self.q = self.build_full_string()
+        
+        
+    def build_filetype_substring(self)->str:
+        '''
+        Docstring
+        '''
+        if len(self.data['filetypes']) > 0:
+            ft_append = " | ".join([f'mime:{f}' for f in self.data['filetypes']])
+            return f"({ft_append})"
+        else:
+            return ''
+        
+    def build_date_substring(self)->str:
+        '''
+        Docstring
+        '''
+        start_dt = self.data["start_date"].replace('-', '') # consider doing this with concatenation
+        end_dt = self.data["end_date"].replace('-', '')
+        
+        if start_dt != "" and end_dt != "":
+            return f'date:{start_dt}..{end_dt}'
+        elif start_dt == "" and end_dt != "":
+            return f'(date:<{start_dt})'
+        elif start_dt != "" and end_dt == "":
+            return f'(date:>{start_dt})'
+        elif start_dt == "" and end_dt == "":
+            return ""
+        else: 
+            print("If this is printed, something went wrong")
 class NERDString:
     
     def __init__(self, text:str, nlp = nlp):
